70e3d0c7f4ad035f5dc1e833ebf3e698bc2b7660
Nick Bell <bell.nicholas.p@gmail.com>
2019-12-02 13:28:30 -0500

    add fetch

---
+++ util/fetch.rb
@@ -0,0 +1,45 @@
+require 'httparty'
+require 'nokogiri'
+
+@day = ARGV.first
+@cookie = File.read("cookie")
+TEMPLATE = <<~RUBY
+@answer1 = nil
+@answer2 = nil
+
+pp @answer1
+pp @answer2
+
+__END__
+RUBY
+
+def get_input
+  HTTParty.get("https://adventofcode.com/2019/day/#{@day}/input", {
+    headers: { 
+      "cookie" => "session=#{@cookie}"
+    }
+  })
+end
+
+def get_title
+  content = HTTParty.get("https://adventofcode.com/2019/day/#{@day}")
+  title = Nokogiri::HTML(content).css("article h2").text
+  filename = title.split(":").last[0..-4]
+  filename.strip.split(" ").map(&:downcase).unshift(@day).join("_") + ".rb"
+end
+
+def create_file
+  if File.exists?(get_title)
+    puts "exists"
+    exit
+  end
+  File.open(get_title, "w+") do |file|
+    file.write(TEMPLATE + get_input)      
+  rescue StandardError
+    "something went wrong"
+  ensure
+    file.close
+  end
+end
+
+create_file
