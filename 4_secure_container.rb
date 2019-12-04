5f802c688c95ef1b36cee6455f55b49dab5fcf49
Nick Bell <bell.nicholas.p@gmail.com>
2019-12-04 00:34:10 -0500

    day 4 prettier

--- 4_secure_container.rb
+++ 4_secure_container.rb
@@ -1,25 +1,20 @@
-@answer1 = nil
-@answer2 = nil
+@answer1 = 0
+@answer2 = 0
 
-input = DATA.each_line.first.split("-").map(&:to_i)
+low, high = DATA.each_line.first.split("-").map(&:to_i)
 
-a1 = []
-a2 = []
-(input[0]..input[1]).each do |inp|
-  if inp.to_s.chars.instance_eval do
-    chunk(&:itself).map(&:last).size < size && sort == self
-  end
-    a1 << inp
-    if inp.to_s.chars.instance_eval { chunk(&:itself).map(&:last).map(&:size).any? { @1 == 2} }
-      a2 << inp
+(low..high).each do |i|
+  digits = i.digits.reverse
+  if digits.uniq != digits && digits.sort == digits
+    @answer1 += 1
+    if digits.chunk(&:itself).any? { |a| a.last.size == 2 }
+      @answer2 += 1
     end
   end
 end
 
-pp a1.uniq.size
-pp a2.uniq.size
-# pp @answer1
-# pp @answer2
+pp @answer1
+pp @answer2
 
 __END__
 357253-892942
