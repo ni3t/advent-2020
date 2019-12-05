b458170b464d531c2f01ccb56e4a26cf308580ce
Nick Bell <bell.nicholas.p@gmail.com>
2019-12-05 03:34:42 -0500

    day 5 done

--- lib/intcode.rb
+++ lib/intcode.rb
@@ -1,43 +1,44 @@
+require "pry"
+
 class Intcode
 
-  attr_accessor :memory
+  attr_accessor :m, :verbose
+
 
   def initialize(memory)
-    @memory = memory.clone
+    @m = memory.clone
   end
+  
 
-  def run
-    catch :run_done do
-      i = 0
-      loop do
-        case memory[i]
-        when 1
-          i = add(i)
-        when 2
-          i = mult(i)
-        when 99
-          throw :run_done
-        else
-          raise "unknown opcode #{memory[i]} at loc #{i}"
-        end
+
+  def run(id)
+    _procs = [
+      ->(*_) { raise "wtf" },
+      ->(m,i,a,b) { m[m[i+3]] = a + b; [i+4,m] }, #add
+      ->(m,i,a,b) { m[m[i+3]] = a * b; [i+4,m] }, #multiply
+      ->(m,i,a,_) { m[i+1] = a; [i+2,m] }, # input
+      ->(m,i,a,_) { puts a unless a.zero?; [i+2,m] }, #output
+      ->(m,i,a,b) { a.zero? ? [i+3,m] : [b,m] }, #jump(T)
+      ->(m,i,a,b) { a.zero? ? [b,m] : [i+3,m] }, #jump(F)
+      ->(m,i,a,b) { m[m[i+3]] = a < b ? 1 : 0; [i+4,m] }, #less than
+      ->(m,i,a,b) { m[m[i+3]] = a == b ? 1 : 0; [i+4,m] } #equals
+    ]
+    i = 0
+    loop do
+      c = m[i] % 100
+      d = m[i].digits # this comes back in this order: code, code, 1p, 2p, 3p
+      a = d[2]&.==(1) ? m[i+1] : m[m[i+1]]
+      b = d[3]&.==(1) ? m[i+2] : m[m[i+2]]
+      pp [i,m[i],c,a,b]
+      puts m[11]
+      puts m[225]
+      case c
+      when 1..2, 4..8; i,m = _procs[c].call(m,i,a,b)
+      when 3; i,m = _procs[c].call(m,i,id,nil)
+      when 99; break
+      else; raise "unknown opcode #{i}"
       end
     end
     self
-  end
-
-  private
-
-  def add(loc)
-    memory[memory[loc+3]] = memory[memory[loc+1]] + memory[memory[loc+2]]
-    loc + 4
-  rescue StandardError
-    raise "Buffer Overflow at #{loc}"
-  end
-
-  def mult(loc)
-    memory[memory[loc+3]] = memory[memory[loc+1]] * memory[memory[loc+2]]
-    loc + 4
-  rescue StandardError
-    raise "Buffer Overflow at #{loc}"
   end
 end
