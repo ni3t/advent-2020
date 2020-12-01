04a75240f046f90aa91a40516001ef9f02be6351
Nick Bell <bell.nicholas.p@gmail.com>
2020-12-01 00:09:38 -0600

    day1

--- util/fetch.rb
+++ util/fetch.rb
@@ -1,50 +1,59 @@
 require 'httparty'
 require 'nokogiri'
 
+@year = 2020
+
 @day = ARGV.first
-@cookie = File.read("cookie")
-TEMPLATE = <<~RUBY
-@answer1 = nil
-@answer2 = nil
+raise 'put in a number dumbass' unless @day
 
-pp @answer1
-pp @answer2
-
-__END__
+@cookie = File.read('cookie')
+TEMPLATE = <<~RUBY.freeze
+  @answer1 = nil
+  @answer2 = nil
+  
+  pp @answer1
+  pp @answer2
+  
+  __END__
 RUBY
 
-def get_input
-  HTTParty.get("https://adventofcode.com/2019/day/#{@day}/input", {
-    headers: { 
-      "cookie" => "session=#{@cookie}"
-    }
-  })
+def fetch_input
+  HTTParty.get("https://adventofcode.com/#{@year}/day/#{@day}/input", {
+                 headers: {
+                   'cookie' => "session=#{@cookie}"
+                 }
+               })
 end
 
-def get_title
-  content = HTTParty.get("https://adventofcode.com/2019/day/#{@day}")
+def fetch_title
+  content = HTTParty.get("https://adventofcode.com/#{@year}/day/#{@day}")
   if content.body =~ /Please don't repeatedly/
-    puts "NOT YET SIR"
+    puts 'NOT YET SIR'
     exit
   end
-  title = Nokogiri::HTML(content).css("article h2").text
-  filename = title.split(":").last[0..-4]
-  filename.strip.split(" ").map(&:downcase).unshift(@day).join("_") + ".rb"
+  title = Nokogiri::HTML(content).css('article h2').text
+  puts title
+  filename = title.split(':').last[0..-4]
+  filename.strip.split(' ').map(&:downcase).unshift(@day).join('_') + '.rb'
 end
 
-def create_file
-  if File.exists?(get_title)
-    puts "exists"
-    exit
-  end
-  File.open(get_title, "w+") do |file|
-    file.write(TEMPLATE + get_input)      
+def write_file
+  File.open(fetch_title, 'w+') do |file|
+    file.write(TEMPLATE + fetch_input)
   rescue StandardError
-    "something went wrong"
+    'something went wrong'
   ensure
     file.close
   end
-  puts "OK GO!"
+end
+
+def create_file
+  if File.exist?(fetch_title)
+    puts 'exists'
+    exit
+  end
+  write_file
+  puts 'OK GO!'
 end
 
 create_file
